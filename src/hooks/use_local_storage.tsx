932eb495fb37d17cc924c96ad459a9ef7e716770
Daniela Kamola <daniela.karwowska@o2.pl>
2022-10-15 11:33:25 +0200

    push before change the branch

--- src/hooks/use_local_storage.tsx
+++ src/hooks/use_local_storage.tsx
@@ -1,16 +1,15 @@
 import { useState } from "react"
 
 const useLocalStorage = (key: string, inicialValue: []) => {
-    
+
     const [value, setValue] = useState(() => {
-        try{
+        try {
             const localStorageJson = localStorage.getItem(key)
             return localStorageJson ? JSON.parse(localStorageJson) : inicialValue
-        }catch (e) {
+        } catch (e) {
             return inicialValue
         }
-    }
-    )
+    })
     const updateValue = (value: any) => {
         setValue(value)
         localStorage.setItem(key, JSON.stringify(value))

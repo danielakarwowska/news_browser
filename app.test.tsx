bd3af98bb75599bf0fb018e5832b96e73c3c7d1b
Daniela Kamola <daniela.karwowska@o2.pl>
2022-11-18 11:17:58 +0100

    run actions

--- app.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import App from './src/app'
-import {render} from '@testing-library/react'
-
-test('renders component', async () => {
-    const {getByText} = render(<App/>)
-    expect(getByText('Hello World')).toBeInTheDocument()
-})

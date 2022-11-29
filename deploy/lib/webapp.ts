a414c8a1043a1266d0916e09c06c41a1a167c011
Daniela Kamola <daniela.karwowska@o2.pl>
2022-11-29 20:05:25 +0100

    cdk_route53

--- deploy/lib/webapp.ts
+++ deploy/lib/webapp.ts
@@ -1,8 +1,26 @@
-import {Stack, StackProps} from '@aws-cdk/core'
+import { Stack, StackProps } from '@aws-cdk/core'
+import * as s3 from '@aws-cdk/aws-s3'
 import * as cdk from '@aws-cdk/core'
+import * as route53 from '@aws-cdk/aws-route53'
+declare const myZone: route53.HostedZone;
 
 export class WebAppStack extends Stack {
    constructor(scope: cdk.Construct, id: string, props?: StackProps) {
       super(scope, id, props)
+
+      new s3.Bucket(this, 'danielakamola.pl', {
+         bucketName: 'danielakamola.pl'
+      })
+      new route53.NsRecord(this, 'NSRecord', {
+         zone: myZone,
+         recordName: 'danielakamola.pl',
+         values: [
+            'ns - 435.awsdns - 54.com',
+            'ns - 1569.awsdns - 04.co.uk',
+            'ns - 1390.awsdns - 45.org',
+            'ns - 807.awsdns - 36.net',
+         ],
+      });
+
    }
 }
